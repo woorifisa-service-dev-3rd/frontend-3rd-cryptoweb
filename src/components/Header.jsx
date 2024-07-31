c517e743b006d2f626b61d939c639e04ae2fae61
hyeri1126 <hyeri1126@ewhain.net>
2024-07-31 12:37:18 +0900

    프로젝트 개발환경 세팅

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div>Crypto</div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/MainBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/MainBody.css'
+
+const MainBody = () => {
+  return (
+    <div className='main'>
+
+    </div>
+  )
+}
+
+export default MainBody
